db0ae5f84c2ae2147cc54ed88d2c13247d3f1fa1
josefinunefaldt <josefin6@hotmail.com>
2024-11-25 11:33:46 +0100

    Display dogs

--- RandomDogs/main.js
+++ RandomDogs/main.js
@@ -8,8 +8,18 @@ document.getElementById("getDogs").addEventListener("click", getdogs);
 
 function getdogs() {
   fetch("https://dog.ceo/api/breeds/image/random")
-  .then((response) => response.json()) 
-  .then((json) => console.dir(json));
-  
+    .then((response) => response.json()) 
+    .then((data) => {
+      console.dir(data); 
+      const dogImageUrl = data.message;
+      const imgElement = document.getElementById("dogImage");
+      imgElement.src = dogImageUrl; 
+      imgElement.style.display = "block";
+    })
+    .catch((error) => {
+      console.error("Error fetching the dog image:", error);
+    });
 }
+
+
 
